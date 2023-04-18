66fa5e02a0d37829c71fc734f157043260f50f2a
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-04-18 19:25:53 +0200

    Init second SpringBoot app

---
+++ OrangeJavaPL2_1/src/main/java/pl/sda/OrangeJavaPL2_1/OrangeJavaPl21Application.java
@@ -0,0 +1,13 @@
+package pl.sda.OrangeJavaPL2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeJavaPl21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeJavaPl21Application.class, args);
+	}
+
+}
